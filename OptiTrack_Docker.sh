7eeaa4ada221dbab12bf4c8db9782496b8458335
0Unkn0wn <mihnea.bastea@gmail.com>
2024-01-21 18:12:20 +0100

    Merged local changes to remote

--- OptiTrack_Docker.sh
+++ OptiTrack_Docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo $'\nAvailable options for ROS2 OptiTrack docker image: \n'
 PS3='Please choose what : '
 options=("Build" "Run" "Quit")
@@ -7,16 +8,17 @@ do
     case $opt in
         "Build")
             echo $'\nBuilding OptiTrack docker image...\n'
-	    sudo docker pull osrf/ros:humble-desktop
-	    sudo docker build -f optitrack_dockerfile -t ros2/optitrack:0.1 .
-	    break
-	    ;;
+            sudo docker pull osrf/ros:humble-desktop
+            sudo docker build -f optitrack_dockerfile -t opti:0.1 .
+            break
+            ;;
         "Run")
             echo $'\nRunning OptiTrack docker image...\n'
-	    sudo docker run -it --rm --net=host --runtime nvidia --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix ros2/optitrack:0.1 bin/bash
-            break
-	    ;;
-	"Quit")
+            sudo docker run -itd --hostname optitrack-docker --add-host="optitrack-docker:127.0.1.1" --add-host="DingoRobot:10.42.0.1" --add-host="l4t-ubuntu:10.42.0.157"  --privileged --rm --net=host --name=Optitrack --runtime nvidia --gpus all -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix opti:0.1 bin/bash && sudo docker exec -it Optitrack sudo route add -net 10.42.0.0 netmask 255.255.255.0 gw 192.168.0.105 && route -n
+            docker attach Optitrack
+             break
+            ;;
+        "Quit")
             break
             ;;
         *) echo "invalid option $REPLY";;
